35804c091057630e6506dd5357c701e1794b8517
clamsawd <clamsawd@openmailbox.org>
2015-10-27 10:50:04 +0100

    Bump up version number to 1.0

--- aria2bt-main.py
+++ aria2bt-main.py
@@ -12,7 +12,7 @@
 #   * https://github.com/tatsuhiro-t/aria2/releases            |
 # Compatible with Python 3.x                                   |
 # --------------------------------------------------------------
-version="0.9-beta"
+version="1.0"
 
 #Import python-modules
 import subprocess
